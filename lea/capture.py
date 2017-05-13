3895d4299943a6fa6b16b58db4bdcc67adc5618b
Leandro <leamotta@msn.com>
2017-05-13 19:18:42 -0300

    change capture

--- lea/capture.py
+++ lea/capture.py
@@ -3,11 +3,6 @@ import sys
 from scapy.all import *
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print ''
-        print "Usage: " + sys.argv[0] + " <interface> <segundos>"
-    else:
-        interface = ""
-        tiempo = int(sys.argv[2])
-        p = sniff(iface = conf.iface, timeout = tiempo, prn = lambda x:x.summary())
-        wrpcap("captura.pcap", p)
+	tiempo = int(sys.argv[1])
+	p = sniff(iface = conf.iface, timeout = tiempo, prn = lambda x:x.summary())
+	wrpcap("captura.pcap", p)
